06ce3728d7365e2a8d912e4f12c97d9a3f94d6e3
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-12-01 18:25:58 +0530

    logic_2_byte

--- DAY1/CASTING_DATA_TYPE/logic_2_byte.sv
+++ DAY1/CASTING_DATA_TYPE/logic_2_byte.sv
@@ -2,13 +2,14 @@
 module casting_logic_byte();
 
   byte byte_data;  
-  bit [3:0] logic_data; 
+  bit [7:0] logic_data; 
 
  initial begin
-   logic_data = 4'b1011xzx;
+   logic_data = 'b1011xzx;
    $display("\nbefore casting logic_data = %0b(%0d)",logic_data, logic_data);
    
    byte_data = byte'(logic_data); // casting logic to byte
    $display("after casting bit to byte = %b(%0d)\n",byte_data, byte_data);
  end
+
 endmodule
