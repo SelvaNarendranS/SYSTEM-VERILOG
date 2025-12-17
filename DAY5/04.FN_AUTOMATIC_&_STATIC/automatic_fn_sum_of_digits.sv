25c99764fefbc6df84440718dc03320bcd25fd66
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-12-17 23:08:26 +0530

    automatic_fn_sum_of_digits

--- DAY5/04.FN_AUTOMATIC_&_STATIC/automatic_fn_sum_of_digits.sv
+++ DAY5/04.FN_AUTOMATIC_&_STATIC/automatic_fn_sum_of_digits.sv
@@ -2,23 +2,24 @@
 // automatic function that calculates the sum of digits of any integer.
 module automatic_function;
   int num;
-  int sum;
+  int sum, count;
   
   initial begin
     repeat(10) begin
       num = $urandom_range(30, 0);
-      sum = sum_digit(num);
-      $display("num = %0d | sum od digits = %0d", num, sum);
+      sum = sum_digit(num, count);
+      $display("num = %0d | sum of digits = %0d | count = %0d", num, sum, count);
     end
   end
   
-  function automatic int sum_digit(int a);
-    int temp;
+  function automatic int sum_digit(int a, output int b);
+    int temp, b = 0;
     while(a !=0) begin
       temp = a % 10;
       sum_digit += temp;
       a /= 10;  
     end
+    b++;
   endfunction
   
 endmodule
