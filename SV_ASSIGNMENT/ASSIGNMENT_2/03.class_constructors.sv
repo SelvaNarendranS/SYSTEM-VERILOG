9046ec08b3e9ae75732827d1892edb5d8462cc81
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-01-20 23:26:11 +0530

    class_constructors

--- SV_ASSIGNMENT/ASSIGNMENT_2/03.class_constructors.sv
+++ SV_ASSIGNMENT/ASSIGNMENT_2/03.class_constructors.sv
@@ -2,10 +2,12 @@
 
 class base;
   int a, b;		// class property
+  logic [7:0]data[]; 	// dynamic array
   
   function new(int a);	// class method -- constructor
     this.a = a;
-    b = 30;			// setting default value at object(memory)allocation
+    b = 30;			// setting default value at object(memory)allocation - initialize static variable
+    data = new[b];		// Allocate memory for dynamic arry
   endfunction
     
 endclass
@@ -18,7 +20,7 @@ module constructors;
     b2 = new(32);			// another constructor
     b2.b = 14; 				// default value over ridding
     
-    $display("Base class b1 a = %0d | b= %0d", b1.a, b1.b);
-    $display("Base class b2 a = %0d | b= %0d", b2.a, b2.b);
+    $display("Base class b1 a = %0d | b= %0d | size of dynamic array = %0d", b1.a, b1.b, b1.data.size());
+    $display("Base class b2 a = %0d | b= %0d | | size of dynamic array = %0d", b2.a, b2.b, b2.data.size());
   end
 endmodule
