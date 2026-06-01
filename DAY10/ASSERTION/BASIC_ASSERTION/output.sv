ccb0bcb2d8ed347b2fbbb15b840a4e25bcfb9c9b
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-06-01 23:47:26 +0530

    output

---
+++ DAY10/ASSERTION/BASIC_ASSERTION/output.sv
@@ -0,0 +1,17 @@
+Info: "design.sv", 19: tb_basic.dut.valid: at time 10 ns
+Test Case is Pass
+Setting in = 1
+Info: "design.sv", 19: tb_basic.dut.valid: at time 30 ns
+Test Case is Pass
+Keeping in = 1
+Info: "design.sv", 19: tb_basic.dut.valid: at time 50 ns
+Test Case is Pass
+Setting in = 0
+Info: "design.sv", 19: tb_basic.dut.valid: at time 70 ns
+Test Case is Pass
+Keeping in = 0
+Info: "design.sv", 19: tb_basic.dut.valid: at time 90 ns
+Test Case is Pass
+Done
+$finish called from file "testbench.sv", line 48.
+$finish at simulation time                   91
