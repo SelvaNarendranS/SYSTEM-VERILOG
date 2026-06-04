2a518e1303451c889f162e144b86047fa1220aa3
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-06-04 23:58:08 +0530

    coverage event trigger sampling
    Implement event trigger sampling in coverage methods.

---
+++ DAY10/COVERAGE/02.COVERGROUP/SAMLING_METHODS/event_trigger_sampling.sv
@@ -0,0 +1,44 @@
+// coverage sampling methods - event trigger
+
+// class
+class parent;
+  event e1;
+  randc bit [2:0] val1;
+  randc bit [1:0] val2;
+
+  // coverage
+  covergroup cg @(e1);			// implicit delay
+    coverpoint val1;
+    value2: coverpoint val2;
+  endgroup
+  
+  function new();
+    cg = new();
+  endfunction
+
+endclass
+
+// testbench
+module sample_methods;
+  
+  parent p1; 		// class handle
+  
+  initial begin
+    p1 = new();		// objection allocation to class
+    
+    repeat(10) begin
+      
+      if(!p1.randomize())
+        $display("randomization error");
+      $display("val1 = %0d | val 2 = %0d", p1.val1, p1.val2);
+      
+      -> p1.e1;		// event trigger for sampling
+      
+      $display("The coverage value = %0.2f%%", p1.cg.get_coverage);
+    end
+    
+    #10;
+    $finish;
+  end
+  
+endmodule
